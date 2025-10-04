39926bd2375f944af9fed0f39816e600728e9b6c
Alfian <145329564+yzn0419@users.noreply.github.com>
2025-10-05 07:00:57 +0800

    Update app.py

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ def set_background(image_file):
     """
     st.markdown(page_bg_img, unsafe_allow_html=True)
 
-set_background("Untitled design (4).jpg")
+set_background("Untitled design (5).jpg")
 
 st.markdown(
     """
@@ -124,6 +124,7 @@ if uploaded_file is not None:
         st.success(label)
     else:
         st.error(label)
+
 
 
 
