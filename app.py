35c9f4d5fab8cdd35d7c38f0452d278c2564cfe9
Alfian <145329564+yzn0419@users.noreply.github.com>
2025-10-05 22:50:09 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,15 +4,15 @@ import pandas as pd
 import tensorflow as tf
 import matplotlib.pyplot as plt
 from tensorflow.keras.models import load_model
-from exoplanet import normalize, pad_or_trim  # reuse functions from exoplanet.py
+from exoplanet import normalize, pad_or_trim  
 import base64
 
-# Function to convert image to base64
+
 def get_base64(bin_file):
     with open(bin_file, "rb") as f:
         return base64.b64encode(f.read()).decode()
 
-# Function to set custom background
+
 def set_background(image_file):
     bin_str = get_base64(image_file)
     page_bg_img = f"""
@@ -29,7 +29,7 @@ def set_background(image_file):
     """
     st.markdown(page_bg_img, unsafe_allow_html=True)
 
-# Set page background
+
 set_background("Untitled design (6).jpg")
 
 st.markdown(
@@ -56,21 +56,19 @@ st.markdown(
 )
 
 
-# Load model
+
 MODEL_PATH = "models/final_model.h5"
 model = load_model(MODEL_PATH)
 
-# Title and description
-st.title("🌌 Exovision")
+
+st.title("🌌 ExoVision")
 st.write("Upload a light curve file (CSV with time + flux columns) to detect possible exoplanet transits.")
 
-# ---------- CUSTOM STYLED Uploader (keeps st.file_uploader backend) ----------
+
 
 uploaded_file = st.file_uploader("Upload light curve CSV", type=["csv"])
-# -------------------------------------------------------------------------
 
 
-# --- File processing logic ---
 if uploaded_file is not None:
     df = pd.read_csv(uploaded_file)
 
@@ -84,7 +82,7 @@ if uploaded_file is not None:
 
     flux_norm = flux / np.nanmedian(flux)
 
-    # Time window slider
+    
     min_time, max_time = float(np.nanmin(time)), float(np.nanmax(time))
     start, end = st.slider(
         "Select time window (days)",
@@ -94,7 +92,7 @@ if uploaded_file is not None:
         step=1
     )
 
-    # Auto-zoom button
+    
     auto_zoom = st.button("🔎 Auto Zoom to Deepest Transit")
     if auto_zoom:
         dip_index = np.nanargmin(flux_norm)
@@ -133,6 +131,7 @@ if uploaded_file is not None:
         st.success(label)
     else:
         st.error(label)
+
 
 
 
